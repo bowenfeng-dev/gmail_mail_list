0bc5a3a9e6f880a042352a8b3c4592bce4aa5c70
Bowen Feng <bowenfeng.dev@gmail.com>
2019-02-02 16:12:12 -0500

    add model data

---
+++ lib/ThreadSummary.dart
@@ -0,0 +1,8 @@
+class ThreadSummary {
+  String from;
+  String subject;
+  String snippet;
+  List<String> attachments;
+
+  ThreadSummary({this.from, this.subject, this.snippet, this.attachments});
+}
